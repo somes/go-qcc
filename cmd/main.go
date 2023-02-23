84e0a56f6eeabaa87d7ba0b213e5a6f4d6316682
somes <example@example.com>
2023-02-23 17:23:25 +0800

    modifed cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 
-	var companyName = "companyName"
+	var companyName = ""
 
 	headers := map[string]string{
 		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
